0e0263da59f6e3d92bb9e3f00e4258d4f10d099f
Kilian Koeltzsch <me@kilian.io>
2017-07-19 18:13:45 +0200

    Add SemesterTests

---
+++ Tests/HTWDresdenTests/SemesterTests.swift
@@ -0,0 +1,44 @@
+import XCTest
+@testable import HTWDresden
+
+class SemesterTests: XCTestCase {
+    func testDeserialization() {
+        let decoder = JSONDecoder()
+        let json = """
+        [
+            "20171",
+            "20162",
+            "20181",
+            "19992"
+        ]
+        """.data(using: .utf8)!
+
+        let semesters = try! decoder.decode([Semester].self, from: json)
+        XCTAssertEqual(semesters[0].description, "SS 2017")
+        XCTAssertEqual(semesters[1].description, "WS 2016/2017")
+        XCTAssertEqual(semesters[2].description, "SS 2018")
+        XCTAssertEqual(semesters[3].description, "WS 1999/2000")
+
+        let invalidYearJSON = "[\"2351\"]".data(using: .utf8)!
+        do {
+            let _ = try decoder.decode([Semester].self, from: invalidYearJSON)
+            XCTFail("Invalid year should not succeed.")
+        }
+        catch {
+            // All good
+        }
+
+        let invalidSemesterId = "[\"20170\"]".data(using: .utf8)!
+        do {
+            let _ = try decoder.decode([Semester].self, from: invalidSemesterId)
+            XCTFail("Invalid semester identifier should not succeed.")
+        }
+        catch {
+            // All good
+        }
+    }
+
+    static var allTests = [
+        ("testDeserialization", testDeserialization)
+    ]
+}
